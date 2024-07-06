5f139e14f5a54f3dced855f06de4b536a93f4a48
liorsze <lior1304@gmail.com>
2024-07-06 18:29:17 +0300

    add Person URI versioning

---
+++ Java Boot Spring Maven/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -0,0 +1,18 @@
+package com.rest.webservices.restful_web_services.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionPerson() {
+        return new PersonV1("Bob Charl");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionPerson() {
+        return new PersonV2(new Name("Bob", "Charl"));
+    }
+}
