c9a596fb45d73edbe58cf3904dfb60a9aa0159cd
liorsze <lior1304@gmail.com>
2024-07-06 12:37:26 +0300

    add handleUserNotFoundExceptions

--- Java Boot Spring Maven/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
+++ Java Boot Spring Maven/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -3,6 +3,7 @@ package com.rest.webservices.restful_web_services.exception;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.rest.webservices.restful_web_services.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,10 +16,19 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	
 	@ExceptionHandler(Exception.class)
 	public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
-		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), 
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
 				ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 		
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundExceptions(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+				ex.getMessage(), request.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+
 	}
 }
