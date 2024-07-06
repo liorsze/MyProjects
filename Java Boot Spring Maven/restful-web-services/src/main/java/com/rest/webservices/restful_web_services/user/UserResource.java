65888a68dcc26d23f32d982ebb6d6eb6995c1742
liorsze <lior1304@gmail.com>
2024-07-06 12:50:30 +0300

    add delete a user

--- Java Boot Spring Maven/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
+++ Java Boot Spring Maven/restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserResource.java
@@ -5,11 +5,7 @@ import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -44,5 +40,11 @@ public class UserResource {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		return ResponseEntity.created(location).build();
+	}
+
+	@DeleteMapping(path = "/users/{id}")
+	public void deleteUser(@PathVariable int id)
+	{
+		service.deleteById(id);
 	}
 }
