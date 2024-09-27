474c4d06c4d3f15ab243177846c7e65a10664e8b
liorsze <lior1304@gmail.com>
2024-09-27 15:16:58 +0300

    add aggregation REST api documentation + refactoring

--- Spring Boot 3 Microservices/api-gateway/src/main/java/com/proj/microservices/api_gateway/routes/Routes.java
+++ Spring Boot 3 Microservices/api-gateway/src/main/java/com/proj/microservices/api_gateway/routes/Routes.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -15,6 +17,14 @@ public class Routes {
     public RouterFunction<ServerResponse> productServiceRoute(){
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
