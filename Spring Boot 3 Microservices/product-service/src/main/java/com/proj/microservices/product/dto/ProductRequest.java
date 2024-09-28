5960aff1eba36bccedc7732cd8a4b254f9e63fac
liorsze <lior1304@gmail.com>
2024-09-28 19:47:23 +0300

    add skuCode to product fields

--- Spring Boot 3 Microservices/product-service/src/main/java/com/proj/microservices/product/dto/ProductRequest.java
+++ Spring Boot 3 Microservices/product-service/src/main/java/com/proj/microservices/product/dto/ProductRequest.java
@@ -2,5 +2,5 @@ package com.proj.microservices.product.dto;
 
 import java.math.BigDecimal;
 
-public record ProductRequest(String id, String name, String description, BigDecimal price) {
+public record ProductRequest(String id, String name, String description, String skuCode, BigDecimal price) {
 }
--- Spring Boot 3 Microservices/product-service/src/main/java/com/proj/microservices/product/model/Product.java
+++ Spring Boot 3 Microservices/product-service/src/main/java/com/proj/microservices/product/model/Product.java
@@ -20,4 +20,5 @@ public class Product {
     private String name;
     private String description;
     private BigDecimal price;
+    private String skuCode;
 }
