5e85254f1e0c4fe71ef6cbaf0a4c84d40584a2b0
liorsze <lior1304@gmail.com>
2024-12-27 16:34:58 +0200

    add audio playback for background music and sound effects in main game loop

--- Rust2/tutorial/src/main.rs
+++ Rust2/tutorial/src/main.rs
@@ -22,7 +22,7 @@ impl Default for GameState {
 }
 fn main() {
     let mut game = Game::new();
-    
+    game.audio_manager.play_music(MusicPreset::Classy8Bit, 0.05);
     // setup game 
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
     player.translation = Vec2::new(0.0, 0.0);
@@ -61,6 +61,7 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 let high_score = engine.texts.get_mut("high_score").unwrap();
                 high_score.value = format!("High Score: {}", game_state.high_score);
             }
+            engine.audio_manager.play_sfx(SfxPreset::Minimize1, 0.1);
         }
     }
     
