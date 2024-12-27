a7569880d19c46d53bb5cab870c098797d084a58
liorsze <lior1304@gmail.com>
2024-12-27 11:30:35 +0200

    implement logging functionality for Frog struct and initialize env_logger

--- Rust2/exercise/logging/src/lib.rs
+++ Rust2/exercise/logging/src/lib.rs
@@ -5,6 +5,7 @@
 // file)
 //
 // Hint: You need to update Cargo.toml to add the `log` dependency, first.
+use log::{error, warn, info, debug, trace};
 
 #[derive(Debug)]
 pub struct Frog {
@@ -15,19 +16,23 @@ pub struct Frog {
 impl Frog {
     pub fn new() -> Self {
         // 2. Use debug!() to log "A new Frog has been created"
+        debug!(target:"Frog::new","A new Frog has been created");
         Default::default()
     }
     pub fn hop(&mut self) {
         self.energy -= 1;
         // 3. Use info!() to log that a Frog hopped, and how much energy is left
+        info!(target:"Frog::hop","A Frog hopped, and now has {} energy left", self.energy);
         if self.energy == 0 {
             // 4. Use warn!() to warn that the frog will go to sleep since he ran out of energy
             self.sleep();
+            warn!(target:"Frog::hop","The Frog ran out of energy and went to sleep");
         }
     }
     pub fn sleep(&mut self) {
         if self.sleeping {
             // 5. Use error!() to log a (non-fatal) error stating that the Frog is already asleep
+            error!(target:"Frog::sleep","The Frog is already asleep");
         } else {
             self.sleeping = true;
         }
@@ -37,9 +42,11 @@ impl Frog {
 impl Default for Frog {
     fn default() -> Self {
         // 6. Use trace!() to log that a default value was generated, with the debug representation
-        Frog {
+        let frog = Frog {
             energy: 5,
             sleeping: false,
-        }
+        };
+        trace!(target: "Frog::Default", "The default Frog is: {:?}", frog);
+        frog
     }
 }
