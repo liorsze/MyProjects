a7742a20790af7f815d37eea2cf236133065163c
liorsze <lior1304@gmail.com>
2024-12-27 17:05:42 +0200

    add functionality to exit game with 'q' key and adjust score text positioning on screen

--- Rust2/tutorial/src/main.rs
+++ Rust2/tutorial/src/main.rs
@@ -1,5 +1,4 @@
 
-use bevy::{audio::PlaybackMode, utils::label};
 use rusty_engine::prelude::*;
 use rand::prelude::*;
 
@@ -22,7 +21,9 @@ impl Default for GameState {
     }
 }
 fn main() {
+
     let mut game = Game::new();
+
     game.audio_manager.play_music(MusicPreset::Classy8Bit, 0.05);
     // setup game 
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
@@ -43,6 +44,20 @@ fn main() {
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    // exit game if q is pressed
+    if engine.keyboard_state.just_pressed(KeyCode::Q){
+        engine.should_exit = true;
+    }
+    
+    // keep text near the edges of the screen
+    let offset = ((engine.time_since_startup_f64 * 4.0).cos()* 5.0) as f32;
+    let score = engine.texts.get_mut("score").unwrap();
+    score.translation.x = engine.window_dimensions.x / 2.0 - 80.0;
+    score.translation.y = engine.window_dimensions.y / 2.0 - 30.0 + offset;
+    let high_score = engine.texts.get_mut("high_score").unwrap();
+    high_score.translation.x = -engine.window_dimensions.x / 2.0 + 110.0;
+    high_score.translation.y = engine.window_dimensions.y / 2.0 - 30.0;
+
     // handle collision events
     //engine.show_colliders = true;
     for event in engine.collision_events.drain(..){
